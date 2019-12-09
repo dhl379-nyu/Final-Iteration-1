1b785c2bb667ba63820c0a03ba7801a9b1dd8340
Dan <dhl379@nyu.edu>
2019-12-08 22:03:03 -0500

    many changes

---
+++ script.js
@@ -0,0 +1,21 @@
+window.addEventListener('scroll', function() {
+  let current_scroll = window.pageYOffset;
+
+  if (current_scroll <= 2600) {
+    document.getElementById('text').innerHTML = "MILLIONS";
+  }
+
+  if (current_scroll > 2600) {
+    document.getElementById('text').innerHTML = "DIE";
+  }
+
+  if (current_scroll > 3400) {
+    document.getElementById('text').innerHTML = "ANUALLY";
+  }
+
+  console.log(current_scroll);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var rellax = new Rellax('.rellax');
+});
